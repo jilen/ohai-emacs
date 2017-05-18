4245e9a5d7c743292a20409a76844bcc489cd2cf
jilen <jilen.zhang@gmail.com>
2017-05-18 18:54:13 +0800

    fix dashbaord

--- modules/ohai-dashboard.el
+++ modules/ohai-dashboard.el
@@ -9,6 +9,7 @@
   (setq dashboard-items '((recents  . 5)
                           (projects . 5)))
   (setq dashboard-startup-banner "~/.emacs.d/logo.png")
+  (setq dashboard-banner-logo-title "Don't forget.\nAlways, somewhere. Someone is fighting for you.\nAs long as you remember her.\nYou are not alone.")
   (dashboard-setup-startup-hook))
 
 (provide 'ohai-dashboard)
