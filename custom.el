bdac9db7e9c140d948a62811b41005fe5c685168
jilen <jilen.zhang@gmail.com>
2018-02-22 14:16:04 +0800

    fix

--- custom.el
+++ custom.el
@@ -10,13 +10,4 @@
  '(ohai-personal-taste/window-state (quote normal))
  '(ohai/modules
    (quote
-    (ohai-appearance ohai-general ohai-dashboard ohai-html ohai-navigation ohai-editing ohai-ac ohai-vue ohai-markdown ohai-javascript ohai-snippets ohai-codestyle ohai-dired ohai-project ohai-ivy ohai-flycheck ohai-git ohai-orgmode ohai-scala ohai-php ohai-help ohai-elisp ohai-fira-code ohai-haskell)))
- '(package-selected-packages
-   (quote
-    (ivy-rich spacemacs-themes spacemacs-theme kaolin-themes counsel-projectile doom-themes window-numbering which-key web-mode volatile-highlights use-package tern swiper-helm spaceline-all-the-icons solarized-theme shrink-whitespace scala-mode sbt-mode rainbow-mode rainbow-delimiters php-mode paredit paradox origami org-plus-contrib org-cliplink org-bullets nyan-mode material-theme markdown-mode magit json-mode js2-refactor intero ibuffer-projectile hlinum highlight-parentheses helm-projectile helm-gtags helm-flycheck helm-flx helm-ext gist flycheck-color-mode-line expand-region exec-path-from-shell ethan-wspace eros discover-my-major dired+ diminish diff-hl dashboard auto-sudoedit auto-complete anzu all-the-icons-dired add-node-modules-path ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    (ohai-gtags ohai-appearance ohai-general ohai-dashboard ohai-html ohai-navigation ohai-editing ohai-complete ohai-vue ohai-markdown ohai-javascript ohai-snippets ohai-codestyle ohai-dired ohai-project ohai-ivy ohai-flycheck ohai-git ohai-orgmode ohai-scala ohai-php ohai-help ohai-elisp ohai-fira-code ohai-haskell))))
